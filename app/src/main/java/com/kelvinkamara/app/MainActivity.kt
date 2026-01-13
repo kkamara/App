bd4142e162226e235d060c93388e4468701e3be9
kkamara <kamaracomputers@gmail.com>
2026-01-13 08:59:05 +0000

    Add a new XML layout and use code to change button text

--- app/src/main/java/com/kelvinkamara/app/MainActivity.kt
+++ app/src/main/java/com/kelvinkamara/app/MainActivity.kt
@@ -2,10 +2,16 @@ package com.kelvinkamara.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.new_layout)
+
+        button = findViewById(R.id.button2)
+        button.text = "Login Button"
     }
 }
