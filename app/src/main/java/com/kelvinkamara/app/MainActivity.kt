007bc90a39465e0d6efd4c5cb4408debb65ae03a
kkamara <kamaracomputers@gmail.com>
2026-01-13 10:51:13 +0000

    Show plain text entered by the user in toggleable text view

--- app/src/main/java/com/kelvinkamara/app/MainActivity.kt
+++ app/src/main/java/com/kelvinkamara/app/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var myButton: Button
     private lateinit var myTextView: TextView
+    private lateinit var editText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +18,20 @@ class MainActivity : AppCompatActivity() {
 
         myButton = findViewById(R.id.myButton)
         myTextView = findViewById(R.id.myTextView)
+        editText = findViewById(R.id.editText)
 
         myButton.text = getString(R.string.button_text)
 
         myButton.setOnClickListener {
-            myTextView.visibility = View.VISIBLE
-            myTextView.text = getString(R.string.text_view_text)
+            if (editText.text.toString().isNotEmpty()) {
+                val input = editText.text.toString()
+
+                myTextView.visibility = View.VISIBLE
+                myTextView.text = input
+            } else {
+                myTextView.visibility = View.VISIBLE
+                myTextView.text = getString(R.string.text_view_text_message)
+            }
         }
     }
 }
